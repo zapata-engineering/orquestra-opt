b0e589e289824709f1c8c66eb2d2704e309d1849
dexter2206 <dexter2206@gmail.com>
2020-11-25 18:32:06 +0100

    Implement converting BQM to serializable

--- src/orquestra/optimization/bqm/io.py
+++ src/orquestra/optimization/bqm/io.py
@@ -1,7 +1,61 @@
-from dimod import BinaryQuadraticModel
+from typing import Dict, Any
+
+import dimod
 from zquantum.core.utils import SCHEMA_VERSION
 from io import TextIOBase
 import json
+
+
+def bqm_to_serializable(bqm: dimod.BinaryQuadraticModel) -> Dict[str, Any]:
+    """Convert binary quadratic model to a serializable dictionary.
+
+    Args:
+        bqm: binary quadratic model to convert
+
+    Returns:
+        Dictionary with the following keys:
+        - linear: list of pairs (i, a_i), where a_i is linear coefficient
+          corresponding to variable i,
+        - quadratic: list of triples (i, j, b_ij) where b_ij is quadratic
+          coefficient corresponding to variables i < j,
+        - offset: offset taken from the model.
+        - vartype: field determining whether variables in the model are
+          from the set {-1, 1} ("SPIN") or the set {0, 1} ("BINARY").
+
+    Notes:
+        A list of tuples was chosen instead of dictionary because dictionary
+        keyed with tuples is not JSON-serializable.
+    """
+    return {
+        "linear": [(label, coef) for label, coef in bqm.linear.items()],
+        "quadratic": [
+            (*sorted((label_1, label_2)), coef)
+            for (label_1, label_2), coef in bqm.quadratic.items()
+        ],
+        "offset": bqm.offset,
+        "vartype": bqm.vartype.name
+    }
+
+
+def bqm_from_serializable(
+    serializable: Dict[str, Any]
+) -> dimod.BinaryQuadraticModel:
+    """Create a binary quadratic model from serializable dictionary.
+
+    Args:
+        serializable: dictionary representing BQM. The expected format
+        of this dictionary as the same as the output format of
+        `bqm_to_serializable`.
+
+    Returns:
+        Binary quadratic model converted from the input dictionary.
+    """
+    return dimod.BinaryQuadraticModel(
+        {i: coef for i, coef in serializable["linear"]},
+        {(i, j): coef for i, j, coef in serializable["quadratic"]},
+        serializable["offset"],
+        vartype=serializable["vartype"]
+    )
 
 
 def load_qubo(input_file):
