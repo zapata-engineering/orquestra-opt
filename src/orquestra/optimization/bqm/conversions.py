70251a1fda9f12f46262f4bebee3636e9b9c0b37
Michał Stęchły <michal.stechly@gmail.com>
2021-02-12 10:28:15 -0500

    176895718 sampleset measurements conversion (#8)
    * Added sampleset as an output of step.
    
    * fixup! Added sampleset as an output of step.
    
    * Added save/load_sampleset functions.
    
    * Fixed imports and simplified steps.
    
    * fixup! Fixed imports and simplified steps.
    
    * Fixed workflow.
    
    * Added more parameters to the example.
    
    * Fixed class name.
    
    * Removed missing import.
    
    * Fixed branch in the workflow.
    
    * Added test for sampleset_io
    
    * Added conversion between SampleSet and Measurements.
    
    * Added assertions to sampleset-measurements conversion.

--- src/orquestra/optimization/bqm/conversions.py
+++ src/orquestra/optimization/bqm/conversions.py
@@ -1,8 +1,13 @@
+import numpy as np
 from openfermion import IsingOperator
-from dimod import BinaryQuadraticModel
+import dimod
+from dimod import BinaryQuadraticModel, SampleSet
+from typing import Optional
+
+from zquantum.core.measurement import Measurements
 
 
-def convert_qubo_to_openfermion_ising(qubo: BinaryQuadraticModel):
+def convert_qubo_to_openfermion_ising(qubo: BinaryQuadraticModel) -> IsingOperator:
     """Converts dimod BinaryQuadraticModel to OpenFermion IsingOperator object.
 
     Args:
@@ -26,7 +31,7 @@ def convert_qubo_to_openfermion_ising(qubo: BinaryQuadraticModel):
     return IsingOperator(ising_string)
 
 
-def convert_openfermion_ising_to_qubo(operator: IsingOperator):
+def convert_openfermion_ising_to_qubo(operator: IsingOperator) -> BinaryQuadraticModel:
     """
     Converts dimod BinaryQuadraticModel to OpenFermion IsingOperator object.
     NOTE: The conversion might not be 100% accurate due to performing floating point operations during conversion between Ising and QUBO models.
@@ -61,3 +66,54 @@ def convert_openfermion_ising_to_qubo(operator: IsingOperator):
         linear_terms, quadratic_terms, offset, vartype="SPIN"
     )
     return dimod_ising.change_vartype("BINARY", inplace=False)
+
+
+def convert_sampleset_to_measurements(sampleset: SampleSet) -> Measurements:
+    """
+    Converts dimod SampleSet to zquantum.core Measurements.
+    Works only for the sampleset with "BINARY" vartype and variables being range of integers starting from 0.
+
+    Note:
+        Since Measurements doesn't hold information about the energy of the samples, this conversion is lossy.
+
+    Args:
+        sampleset: SampleSet we want to convert
+    Returns:
+        Measurements object
+
+    """
+    if sampleset.vartype != dimod.BINARY:
+        raise TypeError("Sampleset needs to have vartype BINARY")
+    for i in range(max(sampleset.variables)):
+        if sampleset.variables[i] != i:
+            raise ValueError(
+                "Variables of sampleset need to be ordered list of integers"
+            )
+
+    bitstrings = [
+        tuple(sample[i] for i in range(len(sample))) for sample in sampleset.samples()
+    ]
+    return Measurements(bitstrings)
+
+
+def convert_measurements_to_sampleset(
+    measurements: Measurements, bqm: Optional[BinaryQuadraticModel] = None
+) -> SampleSet:
+    """
+    Converts dimod SampleSet to zquantum.core Measurements.
+    If no bqm is specified, the vartype of the SampleSet will be "BINARY" and the energies will be NaN.
+    If bqm is specified, its vartype will be preserved and the energy values will be calculated.
+
+    Args:
+        measurements: Measurements object to be converted
+        bqm: if provided, SampleSet will include energy values for each sample.
+    Returns:
+        SampleSet object
+    """
+    if not bqm:
+        return SampleSet.from_samples(
+            measurements.bitstrings, "BINARY", [np.nan for _ in measurements.bitstrings]
+        )
+    if bqm.vartype != dimod.BINARY:
+        raise TypeError("BQM needs to have vartype BINARY")
+    return SampleSet.from_samples_bqm(measurements.bitstrings, bqm)
