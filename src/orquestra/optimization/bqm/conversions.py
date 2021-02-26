fbc90b3090af3ffa23e1b96438c7a32f54e3081d
Michał Stęchły <michal.stechly@gmail.com>
2021-02-26 15:14:52 -0500

    Added change_bitstring_convention to measurement/sampleset conversion. (#11)
    * Added change_bitstring_convention to measurement/sampleset conversion.
    
    * fixup! Added change_bitstring_convention to measurement/sampleset conversion.
    
    * fixup! fixup! Added change_bitstring_convention to measurement/sampleset conversion.

--- src/orquestra/optimization/bqm/conversions.py
+++ src/orquestra/optimization/bqm/conversions.py
@@ -68,16 +68,21 @@ def convert_openfermion_ising_to_qubo(operator: IsingOperator) -> BinaryQuadrati
     return dimod_ising.change_vartype("BINARY", inplace=False)
 
 
-def convert_sampleset_to_measurements(sampleset: SampleSet) -> Measurements:
+def convert_sampleset_to_measurements(
+    sampleset: SampleSet,
+    change_bitstring_convention: bool = False,
+) -> Measurements:
     """
     Converts dimod SampleSet to zquantum.core Measurements.
     Works only for the sampleset with "BINARY" vartype and variables being range of integers starting from 0.
 
     Note:
         Since Measurements doesn't hold information about the energy of the samples, this conversion is lossy.
+        For more explanation regarding change_bitstring_convention please read docs of `convert_measurements_to_sampleset`.
 
     Args:
         sampleset: SampleSet we want to convert
+        change_bitstring_convention: whether to flip the bits in bitstrings to, depends on the convention one is using (see note).
     Returns:
         Measurements object
 
@@ -91,22 +96,41 @@ def convert_sampleset_to_measurements(sampleset: SampleSet) -> Measurements:
             )
 
     bitstrings = [
-        tuple(sample[i] for i in range(len(sample))) for sample in sampleset.samples()
+        tuple(int(change_bitstring_convention != sample[i]) for i in range(len(sample)))
+        for sample in sampleset.samples()
     ]
     return Measurements(bitstrings)
 
 
 def convert_measurements_to_sampleset(
-    measurements: Measurements, bqm: Optional[BinaryQuadraticModel] = None
+    measurements: Measurements,
+    bqm: Optional[BinaryQuadraticModel] = None,
+    change_bitstring_convention: bool = False,
 ) -> SampleSet:
     """
     Converts dimod SampleSet to zquantum.core Measurements.
     If no bqm is specified, the vartype of the SampleSet will be "BINARY" and the energies will be NaN.
     If bqm is specified, its vartype will be preserved and the energy values will be calculated.
 
+    Note:
+        The convention commonly used in quantum computing is that 0 in a bitstring represents eigenvalue 1 of an Ising Hamiltonian,
+        and 1 represents eigenvalue -1.
+        However, there is another convention, used in dimod, where the mapping is 0 -> -1 and 1 -> 1 instead.
+        Therefore if we try to use the bitstrings coming from solving the problem framed in one convention
+        to evaluate energy for problem state in the second one, the results will be incorrect.
+        This might happen in the following scenario:
+        1. Create a qubo
+        2. Solve qubo using dimod solver
+        3. Transform qubo to Ising problem
+        4. Solve Ising problem using QAOA
+        The bitstring coming from steps 2 and 4 will be flipped – 0s and 1s will be exchanged.
+        First convention is the one used in `Measurements` in `zquantum.core` and the second one
+        is used in `SampleSet` in `dimod`. In order to flip the bits in bitstrings, one should use `change_bitstring_convention` flag.
+
     Args:
         measurements: Measurements object to be converted
         bqm: if provided, SampleSet will include energy values for each sample.
+        change_bitstring_convention: whether to flip the bits in bitstrings to, depends on the convention one is using (see note).
     Returns:
         SampleSet object
     """
@@ -116,5 +140,10 @@ def convert_measurements_to_sampleset(
         )
     if bqm.vartype != dimod.BINARY:
         raise TypeError("BQM needs to have vartype BINARY")
-    return SampleSet.from_samples_bqm(measurements.bitstrings, bqm)
 
+    bitstrings = [
+        tuple(int(change_bitstring_convention != bit) for bit in bitstring)
+        for bitstring in measurements.bitstrings
+    ]
+
+    return SampleSet.from_samples_bqm(bitstrings, bqm)
