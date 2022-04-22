0a05aaeefcf4c5502432933f262c339868c427d6
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-04-22 11:27:56 -0400

    removed template files

--- src/orquestra/optimization/optimizers/__init__.py
+++ src/orquestra/optimization/optimizers/__init__.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 from .basin_hopping import BasinHoppingOptimizer
 from .cma_es_optimizer import CMAESOptimizer
 from .grid_search import GridSearchOptimizer
 from .layerwise_ansatz_optimizer import LayerwiseAnsatzOptimizer
+from .optimizer import QiskitOptimizer
 from .scipy_optimizer import ScipyOptimizer
 from .search_points_optimizer import SearchPointsOptimizer
 from .simple_gradient_descent import SimpleGradientDescent
-=======
-from .optimizer import QiskitOptimizer
->>>>>>> 2bfb7237d693b075e922ed8d1ecc8c42b2c43ee9
