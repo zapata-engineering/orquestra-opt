2321703c8167abbbc961f767580f76de589d2599
mstechly <michal.stechly@gmail.com>
2022-05-02 09:21:47 -0400

    chg: remove get_random_hamiltonians_for_problem.

--- src/orquestra/opt/problems/__init__.py
+++ src/orquestra/opt/problems/__init__.py
@@ -2,10 +2,7 @@
 # © Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 from ..api._problem_evaluation import solve_problem_by_exhaustive_search
-from .generators import (
-    get_random_hamiltonians_for_problem,
-    get_random_ising_hamiltonian,
-)
+from .generators import get_random_ising_hamiltonian
 from .graph_partition import GraphPartitioning
 from .max_independent_set import MaxIndependentSet
 from .maxcut import MaxCut
--- tests/orquestra/opt/problems/generators_test.py
+++ tests/orquestra/opt/problems/generators_test.py
@@ -5,50 +5,7 @@ import pytest
 from orquestra.quantum.openfermion import QubitOperator, change_operator_type
 from orquestra.quantum.openfermion.utils import count_qubits
 
-from orquestra.opt.problems import (
-    MaxCut,
-    get_random_hamiltonians_for_problem,
-    get_random_ising_hamiltonian,
-)
-
-
-class TestGenerateRandomHamiltoniansForProblem:
-    def test_get_random_maxcut_hamiltonians_num_instances(self):
-        # Given
-        graph_specs = {"type_graph": "complete"}
-        number_of_instances_list = [0, 1, 10]
-        number_of_qubits = 4
-
-        # When
-        for number_of_instances in number_of_instances_list:
-            hamiltonians = get_random_hamiltonians_for_problem(
-                graph_specs,
-                number_of_instances,
-                number_of_qubits,
-                MaxCut().get_hamiltonian,
-            )
-
-            # Then
-            assert len(hamiltonians) == number_of_instances
-
-    def test_get_random_maxcut_hamiltonians_num_qubits_is_in_range(self):
-        # Given
-        graph_specs = {"type_graph": "complete"}
-        number_of_instances = 10
-        list_possible_number_of_qubits = [[2, 3, 4], [2, 8]]
-
-        # When
-        for possible_number_of_qubits in list_possible_number_of_qubits:
-            hamiltonians = get_random_hamiltonians_for_problem(
-                graph_specs,
-                number_of_instances,
-                possible_number_of_qubits,
-                MaxCut().get_hamiltonian,
-            )
-
-            # Then
-            for hamiltonian in hamiltonians:
-                assert count_qubits(hamiltonian) in possible_number_of_qubits
+from orquestra.opt.problems import MaxCut, get_random_ising_hamiltonian
 
 
 class TestGetRandomIsingHamiltonian:
