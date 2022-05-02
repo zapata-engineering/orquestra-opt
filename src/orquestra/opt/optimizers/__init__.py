3bda78a2d460c1d20f0c8eb68ed141391d24046c
mstechly <michal.stechly@gmail.com>
2022-05-02 08:11:07 -0400

    chg: removed GridSearchOptimizer.

--- src/orquestra/opt/optimizers/__init__.py
+++ src/orquestra/opt/optimizers/__init__.py
@@ -1,6 +1,5 @@
 from .basin_hopping import BasinHoppingOptimizer
 from .cma_es_optimizer import CMAESOptimizer
-from .grid_search import GridSearchOptimizer
 from .qiskit_optimizer import QiskitOptimizer
 from .scipy_optimizer import ScipyOptimizer
 from .search_points_optimizer import SearchPointsOptimizer
--- src/orquestra/opt/optimizers/grid_search/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._grid_search import GridSearchOptimizer
-from ._grid_serialization import load_parameter_grid, save_parameter_grid
-from ._parameter_grid import ParameterGrid, build_uniform_param_grid
