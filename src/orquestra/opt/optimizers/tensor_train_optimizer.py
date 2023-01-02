72c4a3d853fe45aeb08592e678cd5cc5809a6895
Vladimir Vargas Calderón <vladimir.vargas-calderon@zapatacomputing.com>
2023-01-02 07:55:23 -0500

    [docs] adding docs to the functions

--- src/orquestra/opt/optimizers/tensor_train_optimizer.py
+++ src/orquestra/opt/optimizers/tensor_train_optimizer.py
@@ -18,17 +18,20 @@ def _get_n_evaluations_per_candidate(
     evaluation_budget: int, n_rounds: int, n_candidates_per_round: int
 ) -> List[int]:
     """
-    Computes how many evaluations correspond to each candidate in the fairest possible way
-    given an evaluation budget.
+    Computes how many evaluations correspond to each candidate in the fairest possible
+    way given an evaluation budget. The first round always includes only one candidate,
+    which is the initial optimisation. The remaining rounds include
+    `n_candidates_per_round` candidates.
 
     Args:
         evaluation_budget: Total number of evaluations available for all the
             candidates.
-        n_candidates: Number of candidates to distribute the evaluations over.
+        n_rounds: Number of rounds for the optimization.
+        n_candidates_per_round: Number of candidates to evaluate in each round.
 
     Returns:
-        A list of length n_candidates, where each element is the number of evaluations
-        that should be allocated to the corresponding candidate.
+        A list of length (n_rounds - 1) * n_candidates_per_round + 1, with the number of
+        evaluations for each candidate.
     """
     total_candidates = (n_rounds - 1) * n_candidates_per_round + 1
     result = [
@@ -39,8 +42,9 @@ def _get_n_evaluations_per_candidate(
     ]
     if 0 in result:
         raise ValueError(
-            f"Cannot allocate {evaluation_budget} evaluations to {total_candidates} candidates, "
-            f"because at least one candidate would get 0 evaluations. Evaluations are {result}"
+            f"Cannot allocate {evaluation_budget} evaluations to {total_candidates}"
+            "candidates, because at least one candidate would get 0 evaluations. "
+            f"Assigned valuations are {result}"
         )
     return result
 
@@ -49,10 +53,26 @@ def _get_tighter_bounds(
     candidate: np.ndarray,
     bounds: Union[Tuple[float, float], Tuple[np.ndarray, np.ndarray]],
 ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Given a candidate and the bounds it lives in, returns a tighter bounds for the
+    candidate based on how close it is to the boundary of the hyperbox defined by the
+    bounds, and based on the scale of the bounds (i.e. the distance between the lower
+    and upper bounds).
+
+    Args:
+        candidate: An array of parameters.
+        bounds: Either a tuple of floats (lower_bound, upper_bound) or a tuple of arrays
+            (lower_bound, upper_bound), where lower_bound and upper_bound are arrays of
+            the same shape as candidate.
+
+    Returns:
+        A tuple of arrays (lower_bound, upper_bound), where lower_bound and upper_bound
+        are arrays of the same shape as candidate.
+    """
     lower_bound, upper_bound = bounds
     dist_to_lower_bound = candidate - lower_bound
     dist_to_upper_bound = upper_bound - candidate
-    bounds_scale = (upper_bound - lower_bound) / 2
+    bounds_scale = (upper_bound - lower_bound) / 4
     if isinstance(bounds_scale, float):
         bounds_scale = np.ones_like(candidate) * bounds_scale
     closest_distances = np.max(
@@ -69,45 +89,71 @@ class CostFunctionWithBestCandidates:
         n_candidates: int,
         bounds: Union[Tuple[float, float], Tuple[np.ndarray, np.ndarray]],
     ):
+        """
+        A wrapper for the cost function, which will keep information about the best
+        candidates found so far.
+
+        Args:
+            cost_function: A cost function.
+            n_candidates: Number of candidates to keep track of.
+            bounds: Either a tuple of floats (lower_bound, upper_bound) or a tuple of
+            arrays (lower_bound, upper_bound), where lower_bound and upper_bound are
+            arrays of the same shape.
+        """
         self.cost_function = cost_function
         self.candidates: List[dict] = [{}] * n_candidates
         self.candidates_hashes = [0] * n_candidates
         self.bounds = bounds
         self.nfev = 0
+        self.candidate_index = 0
+        self.first_exploration = True
 
     def __call__(self, parameters: np.ndarray) -> float:
         cost = self.cost_function(parameters)
         self.nfev += 1
-        for i, candidate in enumerate(self.candidates):
-            if not candidate or cost <= candidate["cost"]:
-                parameters_hash = hash(parameters.tobytes())
-                if parameters_hash in self.candidates_hashes:
+        if self.first_exploration:
+            for i, candidate in enumerate(self.candidates):
+                if not candidate or cost <= candidate["cost"]:
+                    parameters_hash = hash(parameters.tobytes())
+                    if parameters_hash in self.candidates_hashes:
+                        self.candidates[i]["bounds"] = _get_tighter_bounds(
+                            parameters, self.bounds
+                        )
+                        break
+                    self.candidates.insert(
+                        i,
+                        {
+                            "cost": cost,
+                            "parameters": parameters,
+                            "bounds": _get_tighter_bounds(parameters, self.bounds),
+                        },
+                    )
+                    self.candidates.pop()
+                    self.candidates_hashes.insert(i, parameters_hash)
+                    self.candidates_hashes.pop()
                     break
-                self.candidates.insert(
-                    i,
-                    {
-                        "cost": cost,
-                        "parameters": parameters,
-                        "bounds": _get_tighter_bounds(parameters, self.bounds),
-                    },
+            return cost
+        candidate = self.candidates[self.candidate_index]
+        if not candidate or cost <= candidate["cost"]:
+            parameters_hash = hash(parameters.tobytes())
+            if parameters_hash == self.candidates_hashes[self.candidate_index]:
+                self.candidates[self.candidate_index]["bounds"] = _get_tighter_bounds(
+                    parameters, self.bounds
                 )
-                self.candidates.pop()
-                self.candidates_hashes.insert(i, parameters_hash)
-                self.candidates_hashes.pop()
-                break
+                return cost
+            self.candidates[self.candidate_index] = {
+                "cost": cost,
+                "parameters": parameters,
+                "bounds": _get_tighter_bounds(parameters, self.bounds),
+            }
+            self.candidates_hashes[self.candidate_index] = parameters_hash
         return cost
 
     def gather_best_candidates(self) -> List[dict]:
+        """
+        Creates a list of the best candidates found so far.
+        """
         return [c for c in self.candidates if "parameters" in c.keys()]
-
-    def set_bounds_for_candidate(
-        self, candidate_index: int, bounds: Tuple[np.ndarray, np.ndarray]
-    ):
-        assert (
-            "cost" in self.candidates[candidate_index]
-            and "parameters" in self.candidates[candidate_index]
-        ), "Candidate not initialized"
-        self.candidates[candidate_index]["bounds"] = bounds
 
 
 class TensorTrainOptimizer(Optimizer):
@@ -121,6 +167,28 @@ class TensorTrainOptimizer(Optimizer):
         maximum_number_of_candidates: int = 5,
         recorder: RecorderFactory = _recorder,
     ):
+        """
+        Constructor of a TensorTrainOptimizer. This optimizer uses a tensor-train
+        representation of the cost function to find the minimum of the cost function.
+        It uses the `ttopt` library to do so. This optimizer finds the minimum of the
+        cost function at points given in a grid. This class extends this concept by
+        being able to pick the best candidate solutions in the grid, and then one
+        simply iterates over those candidates on finer grids centered around each
+        candidate to replace each candidate with a better one found in the finer grid.
+
+        Args:
+            n_grid_points: Number of grid points to use in each dimension.
+            n_evaluations: Cost function evaluations budget.
+            bounds: Lower and upper bounds for each parameter.
+            maximum_tensor_train_rank: Maximum bond dimension for the tensor train.
+            n_rounds: Number of optimisation rounds. If 1, then only the initial grid
+                is used. If 2 or more, then the best candidates found in the initial
+                grid are used to create finer grids. Each of these finer grids will
+                get finer and finer depending on the requested number of rounds.
+            maximum_number_of_candidates: Number of candidates to keep track of.
+            recorder Recorder factory for keeping history of calls to the objective
+                function.
+        """
         super().__init__(recorder=recorder)
         self.n_grid_points = n_grid_points
         self.n_evaluations = n_evaluations
@@ -128,7 +196,6 @@ class TensorTrainOptimizer(Optimizer):
         self.maximum_tensor_train_rank = maximum_tensor_train_rank
         self.n_rounds = n_rounds
         self.maximum_number_of_candidates = maximum_number_of_candidates
-        self.candidates = [None] * maximum_number_of_candidates
         self.evaluations_per_candidate = _get_n_evaluations_per_candidate(
             n_evaluations, n_rounds, maximum_number_of_candidates
         )
@@ -136,6 +203,9 @@ class TensorTrainOptimizer(Optimizer):
     def _preprocess_cost_function(
         self, cost_function: CostFunction
     ) -> CostFunctionWithBestCandidates:
+        """
+        Wraps the cost function in a CostFunctionWithBestCandidates.
+        """
         return CostFunctionWithBestCandidates(
             cost_function, self.maximum_number_of_candidates, self.bounds
         )
@@ -163,7 +233,9 @@ class TensorTrainOptimizer(Optimizer):
                 )
                 ttopt.minimize(self.maximum_tensor_train_rank)
             else:
-                for candidate in best_candidates:
+                cost_function.first_exploration = False
+                for idx, candidate in enumerate(best_candidates):
+                    cost_function.candidate_index = idx
                     cost_function.bounds = candidate["bounds"]
                     ttopt = TTOpt(
                         f=cost_function,
