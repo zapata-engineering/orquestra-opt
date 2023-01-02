1db09f09207f73d0eb3315ad5d8b1d1fc6e98fed
Vladimir Vargas Calderón <vladimir.vargas-calderon@zapatacomputing.com>
2023-01-02 08:40:48 -0500

    [test] testing ttopt

---
+++ tests/orquestra/opt/optimizers/tensor_train_optimizer_test.py
@@ -0,0 +1,26 @@
+import pytest
+
+from orquestra.opt.api.optimizer_test import OPTIMIZER_CONTRACTS
+from orquestra.opt.optimizers.tensor_train_optimizer import TensorTrainOptimizer
+
+
+@pytest.fixture(
+    params=[
+        {
+            "n_grid_points": 21,
+            "n_evaluations": 10000,
+            "bounds": (-2.0, 2.0),
+            "maximum_tensor_train_rank": 4,
+            "n_rounds": 3,
+            "maximum_number_of_candidates": 3,
+        }
+    ]
+)
+def optimizer(request):
+    return TensorTrainOptimizer(**request.param)
+
+
+class TestTensorTrainOptimizer:
+    @pytest.mark.parametrize("contract", OPTIMIZER_CONTRACTS)
+    def test_optimizer_satisfies_contracts(self, contract, optimizer):
+        assert contract(optimizer)
