9e28038b1cb36f2d2a18906752214bc72cbd5a1d
Vladimir Vargas Calderón <vladimir.vargas-calderon@zapatacomputing.com>
2023-01-16 09:03:23 -0500

    [test] improving test by checking parameters and value reached

--- tests/orquestra/opt/optimizers/simple_gradient_descent_test.py
+++ tests/orquestra/opt/optimizers/simple_gradient_descent_test.py
@@ -65,10 +65,12 @@ class TestSimpleGradientDescent:
     def test_succeeds_to_minimize_when_cost_function_does_not_have_gradient_method(
         self, optimizer
     ):
-        def cost_function(x):
-            return sum(x)
+        def sum_x_squared_no_gradient(x):
+            return sum(x**2)
 
-        optimizer.minimize(cost_function, np.array([0, 0]))
+        result = optimizer.minimize(sum_x_squared_no_gradient, np.array([1.0, 1.0]))
+        np.testing.assert_almost_equal(result.opt_value, 0)
+        np.testing.assert_almost_equal(result.opt_params, np.array([0, 0]))
 
     def test_history_contains_function_evaluations(self, optimizer, sum_x_squared):
         results = optimizer.minimize(sum_x_squared, np.array([1, 0]), keep_history=True)
